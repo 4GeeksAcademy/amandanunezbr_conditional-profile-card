8514164205588bb02f1ccad7e8c455f621940f56
amandanunezbr <gerencialmonsters@gmail.com>
2024-11-09 15:32:39 +0000

    Cambios en el proyecto

--- src/js/app.js
+++ src/js/app.js
@@ -33,7 +33,7 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${variables.includeCover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastName} ${variables.S.socialMediaPosition}</h1>
+          <h1>${variables.name} ${variables.lastName} ${variables.socialMediaPosition}</h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city} ${variables.country}</h3>
           <ul class="position-left">
